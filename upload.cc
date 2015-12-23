539cb7c52d8e401f25445b41ddd63d1d982b638b
Gangbiao <912273695@qq.com>
2015-12-23 20:48:51 +0800

    odify the file title

--- upload.cc
+++ upload.cc
@@ -2,7 +2,7 @@
 	> File Name: upload.cc
 	> Author: Gangbiao Liu 
 	> Mail: lgb.nwpu@gmail.com
-	> Created Time: Sat 10 Oct 2015 01:20:30 PM EDT
+	> Created Time: Sat 10 Oct 2015 01:20:30 AM CST
  ************************************************************************/
 
 #include<stdio.h>
